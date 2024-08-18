88483b6aff4265edd9f631ed533deb9a70f427b9
John <johnmagdy@rocketmail.com>
2024-08-18 18:30:27 -0400

    path sum

---
+++ trees/path_sum/path_sum.py
@@ -0,0 +1,37 @@
+"""
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+A leaf is a node with no children.
+"""
+from trees.binary_tree import TreeNode
+
+
+class Solution:
+    def has_path_sum(self, root: TreeNode | None, target_sum: int) -> bool:
+        if not root:
+            return False
+        return self._traverse_and_find(root, 0, target_sum)
+
+    def _traverse_and_find(self, node: TreeNode, value_sum: int, target_sum: int) -> bool:
+        if not node:
+            return False
+        value_sum += node.val
+        if not node.left and not node.right:
+            return value_sum == target_sum
+        return self._traverse_and_find(node.left, value_sum, target_sum) or self._traverse_and_find(node.right,
+                                                                                                    value_sum,
+                                                                                                    target_sum)
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    root = TreeNode(5)
+    root.left = TreeNode(4)
+    root.right = TreeNode(8)
+    root.left.left = TreeNode(11)
+    root.left.left.left = TreeNode(7)
+    root.left.left.right = TreeNode(2)
+    root.right.left = TreeNode(13)
+    root.right.right = TreeNode(4)
+    root.right.right.right = TreeNode(1)
+    target_sum = 22
+    print(sol.has_path_sum(root, target_sum))
