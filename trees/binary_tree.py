a6e091169997744873353fd7f8f205c8bd94fef4
John <johnmagdy@rocketmail.com>
2024-08-18 22:30:34 -0400

    traverse trees

--- trees/binary_tree.py
+++ trees/binary_tree.py
@@ -1,6 +1,34 @@
+from typing import List
+
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
+
+class Traverse:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        result = []
+        if root:
+            result.append(root.val)
+            result.extend(self.preorderTraversal(root.left))
+            result.extend(self.preorderTraversal(root.right))
+        return result
+
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        result = []
+        if root:
+            result.extend(self.inorderTraversal(root.left))
+            result.append(root.val)
+            result.extend(self.inorderTraversal(root.right))
+        return result
+
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        result = []
+        if root:
+            result.extend(self.postorderTraversal(root.left))
+            result.extend(self.postorderTraversal(root.right))
+            result.append(root.val)
+        return result
